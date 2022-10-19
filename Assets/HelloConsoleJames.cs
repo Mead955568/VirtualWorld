c98d52f256e9dc2e421e545f7a605637140bf4b5
LukeHoney <2248102@swansea.ac.uk>
2022-10-19 12:57:52 +0100

    Luke Honey Changes

--- Assets/HelloConsoleJames.cs
+++ Assets/HelloConsoleJames.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//write to console Statement From Luke Honey 2248102
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
