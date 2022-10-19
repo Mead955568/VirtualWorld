27b2df9256dea642a8453275597117d7d14a9f6e
LukeHoney <115563029+LukeHoney@users.noreply.github.com>
2022-10-19 17:42:35 +0100

    Added Debug of my Student Number (#4)

--- Assets/HelloConsoleJames.cs
+++ Assets/HelloConsoleJames.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//write to console Statement From Luke Honey 2248102
+//write to console Statement From Luke Honey 2248102 
 //add
 public class NewBehaviourScript : MonoBehaviour
 {
+    //write to console
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("2248102");
     }
 
     // Update is called once per frame
